7dbfb0a4bfd446a0322059b0132ca58cafc242fb
Samidh Patel <spatel13456@gmail.com>
2019-01-24 18:32:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
-    array1 = array.join()
+    array1 = array.join(" , ")
+  elsif array.length == 2
+    array2 = array.join(" and ")
+  else
+    
   
 end
