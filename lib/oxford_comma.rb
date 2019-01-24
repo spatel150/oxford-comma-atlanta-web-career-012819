0cd72f6e54db62e56c5c5e69ad7dac34bc9aee86
Samidh Patel <spatel13456@gmail.com>
2019-01-24 18:39:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
     array1 = array.join(" , ")
+    return array1
   elsif array.length == 2
     array = array.join(" and ")
   elsif array.length > 2
